fb8b199a8fce19cc4900c8d548ab3411ea9860ae
Koh Tong Liang <koh.tongliang@gmail.com>
2025-03-04 18:07:10 +0800

    Organise file structure. Added LayerUtilities tool. Added PlayerINteractions tool

---
+++ ObjectUtilities/DragAndDrop.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameCore
+{
+    public class DragAndDrop : MonoBehaviour
+    {
+        private Vector3 mousePosition;
+        private Camera cam;
+
+        public delegate void OnDragEventStarted();
+        public delegate void OnDragEventStopped();
+
+        public event OnDragEventStarted OnDragStarted;
+        public event OnDragEventStopped OnDragStopped;
+
+        private void Awake()
+        {
+            cam = Camera.main;
+        }
+
+        private Vector3 GetMouseWorldPos()
+        {
+            return cam.WorldToScreenPoint(transform.position);
+        }
+
+        private void OnMouseDown()
+        {
+            OnDragStarted?.Invoke();
+            mousePosition = Input.mousePosition - GetMouseWorldPos();
+        }
+
+        private void OnMouseUp()
+        {
+            OnDragStopped?.Invoke();
+        }
+
+        private void OnMouseDrag()
+        {
+            Vector3 newPos = cam.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+            transform.position = newPos;
+        }
+    }
+}
---
+++ PlayerInteractions/PlayerInteractable.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace GameCore
+{
+    public class PlayerInteractable : MonoBehaviour, IPickable, IInteractable
+    {
+        [SerializeField] private LayerMask targetMask;
+        [SerializeField] private float maxDistance = 2f;
+        public bool interactable = true;
+
+        private PlayerInteractor interactor;
+        private bool hovered = false;
+
+        protected virtual void OnTriggerEnter(Collider other)
+        {
+            if (LayerUtilities.CompareLayerToMask(targetMask, other.gameObject.layer) && interactable)
+            {
+                interactor = other.gameObject.GetComponent<PlayerInteractor>();
+            }
+        }
+
+        protected virtual void OnTriggerExit(Collider other)
+        {
+            if (LayerUtilities.CompareLayerToMask(targetMask, other.gameObject.layer) && interactable)
+            {
+                interactor = other.gameObject.GetComponent<PlayerInteractor>();
+                OnHoverLeave();
+            }
+        }
+
+        private void Update()
+        {
+            if (interactor is null) return;
+            var direction = transform.position - interactor.InteractorView.position;
+            var distance = direction.magnitude;
+            var dotProduct = Vector3.Dot(direction.normalized, interactor.transform.forward);
+            if (dotProduct > 0.5f && distance < maxDistance) // adjust maxDistance to your needs
+            {
+                OnHover();
+            }
+            else
+            {
+                OnHoverLeave();
+            }
+        }
+
+        public virtual void OnHover()
+        {
+            if (hovered) return;
+            hovered = true;
+            interactor.CurrentInteractable = this;
+        }
+
+        public virtual void OnHoverLeave()
+        {
+            if (!hovered) return;
+            hovered = false;
+            interactor.CurrentInteractable = null;
+        }
+
+        public virtual void OnPickup()
+        {
+        }
+
+        public virtual void OnDropped()
+        {
+        }
+
+        public virtual void Interact()
+        {
+            Debug.Log("Interact");
+        }
+    }
+}
